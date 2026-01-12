93b6564fd0e7e61623dc6746c13b7ab717989d5b
Void <lollohol755@gmail.com>
2026-01-13 08:59:24 +0900

    new project

---
+++ src/main/java/com/example/restudy/ReStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.restudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restudy/ReStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
